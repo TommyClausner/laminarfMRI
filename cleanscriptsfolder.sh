0fac5b7485aca9beb08324201e64c3499a185b08
Tommy Clausner <tomcla@mentat004.dccn.nl>
2018-03-11 00:16:31 +0100

    restructering

--- cleanscriptsfolder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-mkdir $DIR/qsuboutput
-mv $DIR/*.e* $DIR/qsuboutput/
-mv $DIR/*.o* $DIR/qsuboutput/
-
--- file2plot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat $1 |gnuplot -p -e 'plot "/dev/stdin" with lines'
