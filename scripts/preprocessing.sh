b9469a50efebf5fc047959d462009b461802ccbb
Tommy Clausner <tomcla@mentat004.dccn.nl>
2018-05-01 17:05:22 +0200

    reset

--- scripts/preprocessing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sh /project/3018037.01/Experiment3.2_ERC/tommys_folder/fMRI_pipeline/P312/B_scripts/do_realignmentQsub.sh
-sh /project/3018037.01/Experiment3.2_ERC/tommys_folder/fMRI_pipeline/P312/B_scripts/do_preparecoregistrationQsub.sh
-sh /project/3018037.01/Experiment3.2_ERC/tommys_folder/fMRI_pipeline/P312/B_scripts/do_distcorrQsub.sh
-sh /project/3018037.01/Experiment3.2_ERC/tommys_folder/fMRI_pipeline/P312/B_scripts/do_correctavgdiff.sh
-sh /project/3018037.01/Experiment3.2_ERC/tommys_folder/fMRI_pipeline/P312/B_scripts/do_applysimpledistcorrQsub.sh
-exit 0
--- scripts/rawScripts/liveupdateqsub.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true
-do
-echo "$(qstat -u $1)"
-sleep 1s
-done
