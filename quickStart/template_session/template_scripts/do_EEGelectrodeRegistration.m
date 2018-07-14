59eff53947beeb5492d54b92042bcac6a9a00d02
TommyClausner <tommy.clausner@gmail.com>
2018-07-14 20:24:29 +0200

    EOD update scripts

--- quickStart/template_session/template_scripts/do_EEGelectrodeRegistration.m
+++ quickStart/template_session/template_scripts/do_EEGelectrodeRegistration.m
@@ -8,6 +8,28 @@ ft_defaults
 
 %% Electrode registration
 
+custom_elecs = 0;
+
+if custom_elecs
+    
+    filetouse=[mainpath filesep '..' filesep 'rawData' filesep 'electrodes' filesep 'photogrammetry' filesep 'electrodes.mat'];
+    load(filetouse)
+    
+    sens=[]
+    sens.chanpos = Electrodes.MRI.points;
+    sens.chantype = arrayfun(@(x) {'eeg'}, 1:size(Electrodes.MRI.points,1))';
+    sens.chanunit = arrayfun(@(x) {'V'}, 1:size(Electrodes.MRI.points,1))';
+    sens.elecpos = Electrodes.MRI.points;
+    sens.label = Electrodes.MRI.label';
+    sens.unit = 'mm'
+    
+    disp(['saving data to ' mainpath filesep '..' filesep '6_EEG' filesep 'sens.mat'])
+    save([mainpath filesep '..' filesep '6_EEG' filesep 'sens.mat'],'sens','-v7.3')
+    disp('done.')
+    
+    exit
+end
+
 headmodelType='FEM';
 
 load([mainpath filesep '..' filesep '6_EEG' filesep 'headmodel_' headmodelType '.mat'])
