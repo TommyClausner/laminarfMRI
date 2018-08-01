700600adcc10f949fd1ecba2ace9b529a5949781
TommyClausner <tommy.clausner@gmail.com>
2018-08-01 12:31:37 +0200

    update scripts

--- quickStart/template_session/template_scripts/make_PRF_overlays.m
+++ quickStart/template_session/template_scripts/make_PRF_overlays.m
@@ -22,7 +22,7 @@ load([mainpath filesep '..' filesep '4_retinotopy' filesep 'images_downsampled.m
 disp('done.')
 %% Ang
 ang_map=nii;
-ang_=resultsall.ang./180.*pi;
+ang_=resultsall.ang./180.*pi; % in rad
 ang_(ang_>(2*pi) | ang_<0)=0;
 ang_map.img(resultsall.options.vxs)=ang_;
 ang_map.img(~resultsall.options.vxs)=0;
@@ -31,7 +31,7 @@ save_untouch_nii(ang_map,[mainpath filesep '..' filesep '4_retinotopy' filesep '
 degvisdeg=params.radius;
 cfactor=2*degvisdeg/size(images{1},2);
 ecc_map=nii;
-ecc_=resultsall.ecc.*cfactor
+ecc_=resultsall.ecc.*cfactor % in degree visual angle
 ecc_(ecc_>degvisdeg | ecc_<0)=0;
 ecc_map.img(resultsall.options.vxs)=ecc_;
 ecc_map.img(~resultsall.options.vxs)=0;
@@ -43,20 +43,20 @@ expt_map.img(~resultsall.options.vxs)=0;
 save_untouch_nii(expt_map,[mainpath filesep '..' filesep '4_retinotopy' filesep 'expt_map.nii']);
 %% rfsize
 rfsize_map=nii;
-rfsize_map.img(resultsall.options.vxs)=resultsall.rfsize.*cfactor;
+rfsize_map.img(resultsall.options.vxs)=resultsall.rfsize.*cfactor; % in degree visual angle
 rfsize_map.img(rfsize_map.img>(2*degvisdeg) | rfsize_map.img<0)=0;
 rfsize_map.img(~resultsall.options.vxs)=0;
 save_untouch_nii(rfsize_map,[mainpath filesep '..' filesep '4_retinotopy' filesep 'rfsize_map.nii']);
 %% R2
 r2_map=nii;
-r2_map.img(resultsall.options.vxs)=resultsall.R2;
-r2_map.img(r2_map.img>100 | r2_map.img<0)=0;
+r2_map.img(resultsall.options.vxs)=resultsall.R2./100;
+r2_map.img(r2_map.img>1 | r2_map.img<0)=0;  % normalized
 r2_map.img(~resultsall.options.vxs)=0;
 save_untouch_nii(r2_map,[mainpath filesep '..' filesep '4_retinotopy' filesep 'r2_map.nii']);
 %% Xpos Ypos
 
-xpos = ecc_ .* cos(ang_)
-ypos = ecc_ .* sin(ang_);
+xpos = ecc_ .* cos(ang_); % in degree visual angle
+ypos = ecc_ .* sin(ang_); % in degree visual angle
 
 xpos_map=nii;
 xpos_map.img(resultsall.options.vxs)=xpos;
