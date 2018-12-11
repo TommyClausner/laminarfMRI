81e3d8045ba823694867b5b067ec758418a2913e
TommyClausner <tommy.clausner@gmail.com>
2018-12-11 12:15:44 +0100

    scripts update

--- quickStart/template_session/template_scripts/do_analyzePRF.sh
+++ quickStart/template_session/template_scripts/do_analyzePRF.sh
@@ -43,7 +43,7 @@ statusqsub=$(qstat $PIDqsub | awk -F' ' '{print $5}' | tail -1)
 
 while [ "$statusqsub" != "C" ]
 do
-sleep 1s
+sleep 60s
 statusqsub=$(qstat $PIDqsub | awk -F' ' '{print $5}' | tail -1)
 
 done
