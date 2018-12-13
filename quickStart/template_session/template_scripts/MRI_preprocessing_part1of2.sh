f7bd56a4cd2b56da491aa3383a35f328f0988b11
TommyClausner <tommy.clausner@gmail.com>
2018-12-13 20:02:02 +0100

    scripts update

--- quickStart/template_session/template_scripts/MRI_preprocessing_part1of2.sh
+++ quickStart/template_session/template_scripts/MRI_preprocessing_part1of2.sh
@@ -2,6 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 echo MRI preprocessing part 1... estimated duration 6h
+sh runonqsub.sh 32gb do_preparefunctionals.sh
+sh waitForQsubPID.sh $(qstat | awk -F' ' '{print $1}' | tail -1)
 echo doing realignment...
 sh runonqsub.sh 32gb do_realignment.sh
 sh waitForQsubPID.sh $(qstat | awk -F' ' '{print $1}' | tail -1)
@@ -15,7 +17,7 @@ sh runonqsub.sh 64gb do_applysimpledistcorr.sh
 sh waitForQsubPID.sh $(qstat | awk -F' ' '{print $1}' | tail -1)
 echo done.
 echo prepare coregistration...
-sh runonqsub.sh 16gb do_preparecoregistration.sh
+sh runonqsub.sh 12:00:00 16gb do_preparecoregistration.sh
 sh waitForQsubPID.sh $(qstat | awk -F' ' '{print $1}' | tail -1)
 sh do_correctavgdiff.sh
 echo done.
